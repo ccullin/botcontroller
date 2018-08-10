9c4c7edd0f054f3a911493121d28c4f2ff6cbb3f
Chris Cullin <chris@miplace.com>
2018-08-10 10:32:19 +0000

    log filename changed

--- logger.py
+++ logger.py
@@ -13,6 +13,6 @@ console.setFormatter(formatter)
 root_logger.addHandler(console)
 
 # Define logging to file.  Use same format as console.
-logfile = RotatingFileHandler('/var/log/webhook.log', maxBytes=5096, backupCount=5)
+logfile = RotatingFileHandler('/var/log/botcontroller/botcontroller.log', maxBytes=5096, backupCount=5)
 logfile.setFormatter(formatter)
 root_logger.addHandler(logfile)
